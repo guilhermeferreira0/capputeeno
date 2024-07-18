2823c8cb737897a8b7ae48c38c74908f0366c9b1
Guilherme Ferreira <guilherme.fsilva23@hotmail.com>
2024-07-18 11:53:25 -0300

    feat: Creating header

--- capputeeno/src/app/layout.tsx
+++ capputeeno/src/app/layout.tsx
@@ -1,6 +1,7 @@
-import { Saira } from 'next/font/google'
-import type { Metadata } from "next";
+import { Header } from "@/components/header";
 import "./globals.css";
+import type { Metadata } from "next";
+import { Saira } from 'next/font/google'
 
 const saira = Saira({
   weight: ['300', '400', '500', '600'],
@@ -19,7 +20,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="pt-br">
-      <body className={saira.className}>{children}</body>
+      <body className={saira.className}>
+        <Header />
+        {children}
+      </body>
     </html>
   );
 }
---
+++ capputeeno/src/components/cart-control.tsx
@@ -0,0 +1,31 @@
+import { useLocalStorage } from "@/hooks/useLocalStorage";
+import { CartIcon } from "./icons/cart-icon";
+import styled from "styled-components";
+
+const CartCount = styled.span`
+  width: 17px;
+  height: 17px;
+  padding: 0px 5px;
+  border-radius: 100%;
+  font-size: 10px;
+  background: var(--delete-color);
+  color: white;
+
+  margin-left: -10px;
+`;
+
+const Container = styled.div`
+  position: relative;
+
+`;
+
+export function CartControl() {
+  const { value } = useLocalStorage('cart-items');
+
+  return (
+    <Container>
+      <CartIcon />
+      {value.length && <CartCount>{value.length}</CartCount>}
+    </Container>
+  );
+}
