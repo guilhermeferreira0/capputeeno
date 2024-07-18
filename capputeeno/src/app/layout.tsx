9871795603ca3ded1e85fff5985332349670c860
Guilherme Ferreira <guilherme.fsilva23@hotmail.com>
2024-07-18 11:04:00 -0300

    Added font saira

--- capputeeno/src/app/layout.tsx
+++ capputeeno/src/app/layout.tsx
@@ -1,5 +1,11 @@
+import { Saira } from 'next/font/google'
 import type { Metadata } from "next";
 import "./globals.css";
+
+const saira = Saira({
+  weight: ['300', '400', '500', '600'],
+  subsets: ['latin']
+})
 
 export const metadata: Metadata = {
   title: "Capputeeno",
@@ -13,7 +19,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="pt-br">
-      <body>{children}</body>
+      <body className={saira.className}>{children}</body>
     </html>
   );
 }
